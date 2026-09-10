fd59bc092d76f0ad5679570b8e4b611e28b72b9c
RenataCortes <cortesrenata1708@gmail.com>
2026-09-09 19:34:36 -0500

    Añadiendo impresión de la operación seleccionada

--- src/calculador.js
+++ src/calculador.js
@@ -1,3 +1,4 @@
 console.log("Calculadora iniciada");
 console.log("Ingrese la operación que desea realizar: suma, resta, multiplicación o división");
 const operacion = prompt("Ingrese la operación que desea realizar: suma, resta, multiplicación o división");
+console.log(`Operación seleccionada: ${operacion}`);
